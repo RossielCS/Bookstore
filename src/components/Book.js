5e5ae2e0f0962d433beec3d23ec353bdb635c0c2
Rossiel Carranza <60085697+RossielCS@users.noreply.github.com>
2020-11-05 08:16:19 -0300

    Implement bookslist component
    - Returns an html table
    - Connects to the redux store

--- src/components/Book.js
+++ src/components/Book.js
@@ -5,17 +5,16 @@ const Book = ({ book }) => {
   const { id, title, category } = book;
 
   return (
-    <tr>
+    <>
       <td>{id}</td>
       <td>{title}</td>
       <td>{category}</td>
-    </tr>
+    </>
   );
 };
 
 Book.propTypes = {
-  // eslint-disable-next-line react/forbid-prop-types
-  book: PropTypes.object.isRequired,
+  book: PropTypes.objectOf(PropTypes.object).isRequired,
 };
 
 export default Book;
---
+++ src/containers/BooksList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Book from '../components/Book';
+
+const BooksList = ({ books }) => {
+  <table>
+    <tr>
+      <th>Book ID</th>
+      <th>Title</th>
+      <th>Category</th>
+    </tr>
+    { books.map(book => (
+      <tr key={book.id}><Book /></tr>))}
+  </table>;
+};
+
+const mapStateToProps = state => ({ books: state });
+
+BooksList.propTypes = {
+  books: PropTypes.objectOf(PropTypes.array).isRequired,
+};
+
+export default connect(mapStateToProps)(BooksList);
