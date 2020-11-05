a4acc53b8312a7e22083eab717384eafef4b5e82
Rossiel Carranza <60085697+RossielCS@users.noreply.github.com>
2020-11-05 08:52:09 -0300

    Implement booksform container
    - Returns a form for adding books
    - book categories

---
+++ src/containers/BooksForm.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const categories = [
+  'Action', 'Biography', 'History',
+  'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+
+const BooksForm = () => {
+  <form>
+    <label htmlFor="title">
+      Title
+      <input type="text" id="title" />
+    </label>
+    <label htmlFor="title">
+      Categories
+      <select>
+        {categories.map(
+          category => <option key={category} value={category}>{category}</option>,
+        )}
+      </select>
+    </label>
+    <input type="submit" value="Submit" />
+  </form>;
+};
+
+export default BooksForm;
