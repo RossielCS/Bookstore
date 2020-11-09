c5dac1df05516b06193c5b80f8a794668e62d7fc
Rossiel <rossielcarranza@gmail.com>
2020-11-09 07:37:46 -0300

    Dispatch changefilter to the redux store
    - Add method handlechangefilter to bookslist component

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const filterCategories = [
-  'Action', 'Biography', 'History',
-  'Horror', 'Kids', 'Learning', 'Sci-Fi'];
-
 const CategoryFilter = ({ handleFilterChange }) => {
-  <label htmlFor="filter">
-    Filter Categories
-    <select onChange={handleFilterChange}>
-      <option key="All" value="All" selected>All</option>
-      {filterCategories.map(
-        category => (
-          <option
-            key={category}
-            value={category}
-          >
-            {category}
-          </option>
-        ),
-      )}
-    </select>
-  </label>;
+  const filterCategories = [
+    'Action', 'Biography', 'History',
+    'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+  return (
+    <label htmlFor="filter">
+      Filter Categories
+      <select onChange={e => handleFilterChange(e)}>
+        <option key="All" value="All" selected>All</option>
+        {filterCategories.map(
+          category => (
+            <option
+              key={category}
+              value={category}
+            >
+              {category}
+            </option>
+          ),
+        )}
+      </select>
+    </label>
+  );
 };
 
 CategoryFilter.propTypes = {
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import booksReducer from './books';
+import filtersReducer from './filter';
 
 const rootReducer = combineReducers({
   books: booksReducer,
+  filters: filtersReducer,
 });
 
 export default rootReducer;
